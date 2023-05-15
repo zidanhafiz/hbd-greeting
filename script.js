cfad3d92386d8132c19cbed11138c4ced5f85c88
zidanhafiz <nadizsonata@gmail.com>
2023-05-15 13:15:01 +0700

    - Fix some Bugs - Change the music background - Adding favicon - Adding overlay - Making an auto show Ayaka's greet

--- script.js
+++ script.js
@@ -1,52 +1,29 @@
 const paragraph = document.querySelectorAll(".paragraph");
 const modal = document.querySelector(".modal");
 const audio = document.querySelector(".backsound");
+const container = document.querySelector(".container");
 
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
 const title = document.querySelector(".title");
 
+const overlay = document.getElementById("popup1");
+
 const animeGreet = `<div class="anime-greet">
-<div class="close-btn"><i class="fa-regular fa-circle-xmark" style="color: #ffffff"></i></div>
-<div class="ayaka">
-  <audio autoplay src="./src/ayaka-hbd-2.wav"></audio>
-  <div class="pict">
-    <img src="./src/ayakagif.gif" alt="ayaka" />
-  </div>
-  <div class="text">
-    <p>Selamat ulang tahun, Akhza-san!</p>
-    <p>Semoga tetap sehat dan panjang umur</p>
-    <p>Kamisato Ayaka</p>
-    <p>Istri Zidan 😊</p>
-  </div>
-</div>
-</div>`;
-
-let i = 0;
-
-nextBtn.addEventListener("click", function () {
-  if (i < 26) {
-    paragraph[i].classList.toggle("hidden");
-    i++;
-    paragraph[i].classList.toggle("hidden");
-  } else if (i >= 26) {
-    modal.innerHTML = animeGreet;
-    audio.pause();
-    const closeBtn = document.querySelector(".close-btn");
-    closeBtn.addEventListener("click", () => {
-      modal.innerHTML = "";
-      audio.play();
-    });
-  }
-});
-
-prevBtn.addEventListener("click", function () {
-  if (i > 0) {
-    paragraph[i].classList.toggle("hidden");
-    i--;
-    paragraph[i].classList.toggle("hidden");
-  }
-});
+                    <div class="close-btn"><i class="fa-regular fa-circle-xmark" style="color: #ffffff"></i></div>
+                    <div class="ayaka">
+                      <audio autoplay src="./src/ayaka-hbd-2.wav"></audio>
+                      <div class="pict">
+                        <img src="./src/ayakagif.gif" alt="ayaka" />
+                      </div>
+                      <div class="text">
+                        <p>Selamat ulang tahun, Akhza-san!</p>
+                        <p>Semoga tetap sehat dan panjang umur</p>
+                        <p>Kamisato Ayaka</p>
+                        <p>Istri Zidan 😊</p>
+                      </div>
+                    </div>
+                    </div>`;
 
 // script for ballon
 const balloonContainer = document.getElementById("balloon-container");
@@ -87,10 +64,73 @@ function removeBalloons() {
   }, 500);
 }
 
-window.addEventListener("load", () => {
+overlay.addEventListener("click", () => {
+  overlay.classList.add("hidden-fadeOut");
+  setTimeout(() => {
+    overlay.classList.remove("hidden-fadeOut");
+    overlay.classList.add("hidden");
+  }, 1300);
+  container.classList.remove("hidden");
   createBalloons(30);
+  audio.play();
 });
 
-window.addEventListener("click", () => {
+window.addEventListener("load", () => {
+  container.classList.add("hidden");
+});
+
+balloonContainer.addEventListener("click", () => {
   removeBalloons();
+});
+
+let i = 0;
+let j = 0;
+let k = 0;
+
+window.addEventListener("click", () => {
+  if (i >= 26) {
+    j++;
+    if (j < 2) {
+      k = 1;
+      setTimeout(() => {
+        modal.innerHTML = animeGreet;
+        audio.volume = 0.3;
+        const closeBtn = document.querySelector(".close-btn");
+        closeBtn.addEventListener("click", () => {
+          modal.innerHTML = "";
+          audio.volume = 1;
+        });
+        k = 0;
+      }, 3000);
+    }
+  }
+});
+
+nextBtn.addEventListener("click", function () {
+  if (i < 26) {
+    paragraph[i].classList.toggle("hidden");
+    paragraph[i].classList.toggle("now");
+    i++;
+    paragraph[i].classList.toggle("hidden");
+    paragraph[i].classList.toggle("now");
+  } else if (i >= 26) {
+    if (k === 0) {
+      modal.innerHTML = animeGreet;
+      audio.volume = 0.3;
+      const closeBtn = document.querySelector(".close-btn");
+      closeBtn.addEventListener("click", () => {
+        modal.innerHTML = "";
+        audio.volume = 1;
+      });
+    }
+  }
+});
+
+prevBtn.addEventListener("click", function () {
+  if (i > 0) {
+    paragraph[i].classList.toggle("hidden");
+    i--;
+    paragraph[i].classList.toggle("hidden");
+    j = 0;
+  }
 });
